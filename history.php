15b18fbc0daadf3936d048412b9c2a3f90ca4999
edongjoselito <joselito.edong@candortci.edu.ph>
2025-07-08 11:11:34 +0800

    commit

---
+++ history.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html class="wide wow-animation scrollTo" lang="en">
+
+<head>
+    <title>History</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="keywords" content="intense web design multipurpose template">
+    <meta name="date" content="Dec 26">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans:400,300italic,300,400italic,600,700%7CMerriweather:400,300,300italic,400italic,700,700italic">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/novi.css">
+    <!--[if lt IE 10]><div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div><script src="js/html5shiv.min.js"></script><![endif]-->
+</head>
+
+<body>
+    <div class="page-loader">
+        <div class="page-loader-body"><span class="cssload-loader"><span class="cssload-loader-inner"></span></span></div>
+    </div>
+    <div class="page text-center">
+        <?php include('includes/header.php'); ?>
+
+        <section class="section breadcrumb-modern context-dark parallax-container" data-parallax-img="images/parallax-03.jpg">
+            <div class="parallax-content section-30 section-sm-70">
+                <div class="shell">
+                    <h2 class="veil reveal-sm-block">History</h2>
+                    <div class="offset-sm-top-35">
+                        <ul class="list-inline list-inline-lg list-inline-dashed p">
+                            <li><a href="index.html">Home</a></li>
+                            <li>History</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section novi-background bg-cover section-70 section-md-114 bg-default">
+            <div class="shell">
+                <div class="range range-50">
+                    <div class="cell-sm-4 cell-sm-push-2 text-sm-left">
+                        <div class="inset-sm-left-30"><img class="img-responsive reveal-inline-block img-rounded" src="images/users/user-christopher-smith-340x300.jpg" width="340" height="300" alt="">
+                            <div class="offset-top-20">
+                                <h6 class="text-primary text-bold">Christopher Smith</h6>
+                            </div>
+                            <p>First president of the James Whitaker University, served from 1876 to 1897.</p>
+                        </div>
+                    </div>
+                    <div class="cell-sm-8 cell-sm-push-1 text-sm-left">
+                        <h2 class="text-bold">A Meeting of Minds</h2>
+                        <hr class="divider bg-madison hr-sm-left-0">
+                        <div class="offset-top-30 offset-sm-top-30">
+                            <p>The original board of trustees laid the groundwork for what would become today's James Whitaker University on July 9, 1876, when the group gathered to hold the University's inaugural board meeting and drafted the University's articles of incorporation. This guiding document outlined elements the founders believed would build an enduring legacy for the University: a commitment to offering a rigorous academic program and an ambition to provide "opportunities for all departments of higher education to persons of both sexes on equal terms." On September 10, 1876, the State of California issued the University's official certificate of incorporation, marking the formal beginning of the University's life.</p>
+                        </div>
+                        <p>An initial pledge of $600,000 (roughly $16 million in today's currency) from oil magnate James Whitaker, along with contributions by the American Baptist Education Society, helped to found the University. The University's land was donated by Marshall Field, owner of the historic Chicago department store that bore his name.</p>
+                        <p>Christopher Smith, the University's first president, envisioned a university that was "bran splinter new, yet as solid as the ancient hills" - a modern research university..</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+      
+        <section class="section novi-background bg-cover section-70 section-md-114 bg-default">
+            <div class="shell">
+                <h2 class="text-bold">Statistics</h2>
+                <hr class="divider bg-madison">
+                <div class="range range-65 range-xs-center range-md-left offset-top-55 counters">
+                    <div class="cell-sm-6 cell-md-3">
+                        <div class="counter-type-1"><span class="icon novi-icon icon-lg icon-outlined text-madison mdi mdi-school"></span>
+                            <div class="h3 text-bold text-primary offset-top-15"><span class="counter">97</span><span class="symbol">%</span></div>
+                            <hr class="divider bg-gray-light divider-sm">
+                            <div class="offset-top-10">
+                                <h6 class="text-black font-accent">Graduates</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cell-sm-6 cell-md-3">
+                        <div class="counter-type-1"><span class="icon novi-icon icon-lg icon-outlined text-madison mdi mdi-wallet-travel"></span>
+                            <div class="h3 text-bold text-primary offset-top-15"><span class="counter">30</span><span class="symbol">+</span></div>
+                            <hr class="divider bg-gray-light divider-sm">
+                            <div class="offset-top-10">
+                                <h6 class="text-black font-accent">Certified Teachers</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cell-sm-6 cell-md-3">
+                        <div class="counter-type-1"><span class="icon novi-icon icon-lg icon-outlined text-madison mdi mdi-domain"></span>
+                            <div class="h3 text-bold text-primary offset-top-15"><span class="counter">8</span><span class="symbol"></span></div>
+                            <hr class="divider bg-gray-light divider-sm">
+                            <div class="offset-top-10">
+                                <h6 class="text-black font-accent">Student Campuses</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cell-sm-6 cell-md-3">
+                        <div class="counter-type-1"><span class="icon novi-icon icon-lg icon-outlined text-madison mdi mdi-account-multiple-outline"></span>
+                            <div class="h3 text-bold text-primary offset-top-15"><span class="counter">6510</span><span class="symbol"></span></div>
+                            <hr class="divider bg-gray-light divider-sm">
+                            <div class="offset-top-10">
+                                <h6 class="text-black font-accent">Students</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php include('includes/footer.php'); ?>
+    </div>
+    <!-- <div class="snackbars" id="form-output-global"></div><a class="builder-button" href="http://whizthemes.com/builder/b_patshala/patshala-builder" target="_blank"><img src="images/novi-icon-26x26.png" alt="" width="26" height="26">Try builder</a> -->
+    <div class="snackbars" id="form-output-global"></div>
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
